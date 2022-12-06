c10fbaa6f36c9934efc851fc00f55ae6e68b9f1c
daviddgamble <daviddgamble@gmail.com>
2022-12-06 10:01:25 -0800

    Adds TriangleType() with good fail for equilateral.

--- TriangleTracker.Tests/ModelTests/TriangleTrackerTests.cs
+++ TriangleTracker.Tests/ModelTests/TriangleTrackerTests.cs
@@ -12,5 +12,21 @@ namespace TriangleTracker.Tests
       Tracker testTracker = new Tracker();
       Assert.AreEqual(true, testTracker.IsTriangle(7, 10, 5));
     }
+
+    [TestMethod]
+
+    public void IsTriangle_ThreeGivenSidesNotTriangle_False()
+    {
+      Tracker testTracker = new Tracker();
+      Assert.AreEqual(false, testTracker.IsTriangle(3, 3, 10));
+    }
+
+    [TestMethod]
+
+    public void TriangleType_GivenSidesMakeEquilateral_Equilateral()
+    {
+      Tracker testTracker = new Tracker();
+      Assert.AreEqual("equilateral", testTracker.TriangleType(5, 5, 5));
+    }
   }
 }
--- TriangleTracker/Models/TriangleTracker.cs
+++ TriangleTracker/Models/TriangleTracker.cs
@@ -4,14 +4,19 @@ namespace TriangleTracker
   {
     public bool IsTriangle(int side1, int side2, int side3)
     {
-      if (side1 + side2 > side3)
-      {
-        return true;
-      }
-      else
+      if (side1 + side2 < side3 || side1 + side3 < side2 || side2 + side3 < side1)
       {
         return false;
       }
+      else
+      {
+        return true;
+      }
     }
+
+    public string TriangleType(int side1, int side2, int side3)
+    {
+      return "test";
+    } 
   }
 }
