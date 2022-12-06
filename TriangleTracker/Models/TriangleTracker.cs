3d5f8fda7a9bbe166ac0b81d133a5ce0dd988b6e
daviddgamble <daviddgamble@gmail.com>
2022-12-06 09:42:30 -0800

    Add IsTriangle() test passed.

--- TriangleTracker/Models/TriangleTracker.cs
+++ TriangleTracker/Models/TriangleTracker.cs
@@ -4,7 +4,14 @@ namespace TriangleTracker
   {
     public bool IsTriangle(int side1, int side2, int side3)
     {
-      return false;
+      if (side1 + side2 > side3)
+      {
+        return true;
+      }
+      else
+      {
+        return false;
+      }
     }
   }
 }
